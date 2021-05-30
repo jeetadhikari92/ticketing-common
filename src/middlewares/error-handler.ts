3dc325e20fb4dd377bfeebd87782f8af98e6428a
jeet adhikari <jeet@backbase.com>
2021-05-30 18:43:25 +0200

    update error loggin for unknown errors

--- src/middlewares/error-handler.ts
+++ src/middlewares/error-handler.ts
@@ -10,6 +10,7 @@ export const errorHandler = (
   if(err instanceof CustomError) {
     return res.status(err.statusCode).send({errors: err.serializeErrors()})
   }
+  console.error(err);
   res.status(500).send({ errors: [
       { message: 'Something went wrong.' }
     ]
