8eeb91730c7d8849e24213c16590d6e040d5ced0
jeet adhikari <jeet@backbase.com>
2021-06-05 22:02:28 +0200

    added order events

--- src/index.ts
+++ src/index.ts
@@ -15,5 +15,7 @@ export * from "./events/publisher"
 export * from "./events/subjects"
 export * from "./events/ticket/ticket-created-event"
 export * from "./events/ticket/ticket-updated-event"
+export * from "./events/order/order-created-event"
+export * from "./events/order/order-cancelled-event"
 
 export * from "./types/order-status"
